3cc8c0b9d5f54083075630f411f7576856e5eb69
divakar6 <divakar.r2023cse@sece.ac.in>
2025-01-22 08:53:05 +0530

    done task

--- src/components/Signup.jsx
+++ src/components/Signup.jsx
@@ -1,8 +1,26 @@
+import { Link } from "react-router-dom"
+import "../../css/signUp.css"
 const Signup=()=>{
-    return(
-        <div>
-        <h1>Welcome to Signup page</h1>
+    return (
+    
+        <div className="signup">
+            <form>
+                <div className="input">
+                    <label htmlFor="name">Name</label>
+                    <input type="text" name="name" placeholder="enter your name"/>
+                </div>
+                <div className="input">
+                    <label htmlFor="email">Email</label>
+                    <input type="text" name="email" placeholder="enter your email" />
+                </div>
+                <div className="input">
+                    <label htmlFor="password">Password</label>
+                    <input type="text" name="password" placeholder="enter your password"/>
+                </div>
+                <Link to='/home' className="btn">Submit</Link>
+            </form>
         </div>
+        
     )
 }
 export default Signup
